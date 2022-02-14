1ce11d1636ba16e8e1c7eb11a65d36af269fbad8
madjiebimaa <madjiebimaa@gmail.com>
2022-02-14 23:33:54 +0700

    chore: add docker-compose.yaml file

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"fcc-request-header-parser-ms/handlers"
+	"fcc-request-header-parser-ms/middlewares"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.New()
+	r.Use(gin.Logger(), gin.Recovery(), middlewares.CORS())
+
 	r.GET("/api/whoami", handlers.WhoAmIHandler)
-	r.Run(":3000")
+
+	if err := r.Run(":3000"); err != nil {
+		log.Fatal("can't connect to server at port 3000")
+	}
 }
